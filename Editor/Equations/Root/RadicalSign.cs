b64e591fba3f5e704b15549b48b8467233316041
Jack251970 <1160210343@qq.com>
2025-10-19 22:58:04 +0800

    Code cleanup

--- Editor/Equations/Root/RadicalSign.cs
+++ Editor/Equations/Root/RadicalSign.cs
@@ -5,7 +5,8 @@ namespace Editor
 {
     public sealed class RadicalSign : EquationBase
     {
-        static double widthFactor = .9;
+        private static readonly double widthFactor = .9;
+
         public RadicalSign(EquationContainer parent)
             : base(parent)
         {
@@ -15,10 +16,7 @@ namespace Editor
 
         public override double FontSize
         {
-            get
-            {
-                return base.FontSize;
-            }
+            get => base.FontSize;
             set
             {
                 base.FontSize = value;
@@ -29,30 +27,30 @@ namespace Editor
         public override void DrawEquation(DrawingContext dc)
         {
             dc.DrawPolyline(new Point(Left, Bottom - Height * .4),
-                                   [
-                                        new Point(Left + FontSize * .2, Bottom - Height * .5),
-                                        new Point(Left + FontSize * .2, Bottom - Height * .5),
-                                        new Point(Left + FontSize * .4, Bottom),
-                                        new Point(Left + FontSize * .4, Bottom),
-                                        new Point(Right - FontSize * .1, Top + FontSize * .1),
-                                        new Point(Right - FontSize * .1, Top + FontSize * .1),
-                                        new Point(ParentEquation.Right, Top + FontSize * .1),
-                                    ],
-                                   StandardPen);
+                [
+                    new Point(Left + FontSize * .2, Bottom - Height * .5),
+                    new Point(Left + FontSize * .2, Bottom - Height * .5),
+                    new Point(Left + FontSize * .4, Bottom),
+                    new Point(Left + FontSize * .4, Bottom),
+                    new Point(Right - FontSize * .1, Top + FontSize * .1),
+                    new Point(Right - FontSize * .1, Top + FontSize * .1),
+                    new Point(ParentEquation.Right, Top + FontSize * .1),
+                ],
+                StandardPen);
         }
 
         //An incomplete manual solution
         //new PointCollection 
-        //                           {   
-        //                               new Point(Left + FontSize * .2, Bottom - Height * .5),                                         
-        //                               new Point(Left + FontSize * .4, Bottom - FontSize * .17),
-        //                               new Point(Right - FontSize * .1, Top + FontSize * .1),
-        //                               new Point(ParentEquation.Right, Top + FontSize * .1),
-        //                               new Point(ParentEquation.Right, Top + FontSize * .17),
-        //                               new Point(Right - FontSize * .05, Top + FontSize * .17),
-        //                               new Point(Left + FontSize * .41, Bottom),
-        //                               new Point(Left + FontSize * .37, Bottom),
-        //                               new Point(Left + FontSize * .2, Bottom - Height * .45),
-        //                           });
+        //{   
+        //    new Point(Left + FontSize* .2, Bottom - Height* .5),                                         
+        //    new Point(Left + FontSize* .4, Bottom - FontSize* .17),
+        //    new Point(Right - FontSize* .1, Top + FontSize* .1),
+        //    new Point(ParentEquation.Right, Top + FontSize* .1),
+        //    new Point(ParentEquation.Right, Top + FontSize* .17),
+        //    new Point(Right - FontSize* .05, Top + FontSize* .17),
+        //    new Point(Left + FontSize* .41, Bottom),
+        //    new Point(Left + FontSize* .37, Bottom),
+        //    new Point(Left + FontSize * .2, Bottom - Height * .45),
+        //});
     }
 }
