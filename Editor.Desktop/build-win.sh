309db596db9940dbf723d2949258ed15d0b88a84
Jack251970 <1160210343@qq.com>
2025-11-14 13:21:23 +0800

    Add build scripts for Linux, macOS, and Windows
    Added `build-linux.sh` to automate the build process for Linux,
    including cleaning, publishing, and packaging with Velopack.
    
    Added `build-osx.sh` to automate the build process for macOS,
    with similar functionality as the Linux script.
    
    Added `build-win.bat` for Windows builds using a batch script,
    supporting publishing and packaging with Velopack.
    
    Added `build-win.sh` for Windows builds using a Bash script,
    providing an alternative to the batch script with additional
    cleanup functionality.

---
+++ Editor.Desktop/build-win.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Find the absolute path of the script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Check if version parameter is provided
+if [ "$#" -ne 1 ]; then
+    echo "Version number is required."
+    echo "Usage: ./build.sh [version]"
+    exit 1
+fi
+
+BUILD_VERSION="$1"
+RELEASE_DIR="$SCRIPT_DIR/releases"
+PUBLISH_DIR="$SCRIPT_DIR/publish"
+ICON_PATH="$SCRIPT_DIR/icon.ico"
+
+echo "Cleaning up previous build..."
+dotnet clean
+echo ""
+echo "Compiling MathEditor with dotnet..."
+dotnet publish -c Release --self-contained -r win-x64 -o "$PUBLISH_DIR"
+
+echo ""
+echo "Building Velopack Release v$BUILD_VERSION"
+vpk [win] pack --runtime win-x64 -u Editor.Desktop --packTitle "MathEditor" -v $BUILD_VERSION -o "$RELEASE_DIR" -p "$PUBLISH_DIR" -e "Editor.Desktop.exe" -i "$ICON_PATH"
